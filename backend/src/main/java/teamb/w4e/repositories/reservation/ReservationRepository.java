207045e317d482a253a0ca1302885104d8fe902f
Orfila Marie <marie.orfila@etu.univ-cotedazur.fr>
2024-02-21 14:54:03 +0100

    feat: find reservation by customer #9

--- backend/src/main/java/teamb/w4e/repositories/reservation/ReservationRepository.java
+++ backend/src/main/java/teamb/w4e/repositories/reservation/ReservationRepository.java
@@ -1,14 +1,16 @@
 package teamb.w4e.repositories.reservation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import teamb.w4e.entities.Card;
 import teamb.w4e.entities.Reservation;
 
 import java.util.List;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    List<Reservation> findReservationByCard(Long cartId);
+    @Query("SELECT r FROM Reservation r WHERE r.card.id = :cardId")
+    List<Reservation> findReservationByCard( @Param("cardId") Long cartId);
 
 }
