a57101a4222fba5bb8abed8eb5be51cc547ed0ab
Orfila Marie <marie.orfila@etu.univ-cotedazur.fr>
2024-02-22 11:57:20 +0100

    fix: merge errors #7

--- backend/src/main/java/teamb/w4e/components/Cashier.java
+++ backend/src/main/java/teamb/w4e/components/Cashier.java
@@ -4,6 +4,8 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import teamb.w4e.entities.Customer;
+import teamb.w4e.entities.Item;
+import teamb.w4e.entities.Reservation;
 import teamb.w4e.entities.Transaction;
 import teamb.w4e.exceptions.CustomerIdNotFoundException;
 import teamb.w4e.exceptions.NegativeAmountTransactionException;
@@ -12,6 +14,7 @@ import teamb.w4e.interfaces.Bank;
 import teamb.w4e.interfaces.CustomerFinder;
 import teamb.w4e.interfaces.Payment;
 import teamb.w4e.interfaces.TransactionCreator;
+import teamb.w4e.interfaces.reservation.ReservationCreator;
 
 @Service
 public class Cashier implements Payment {
@@ -19,11 +22,14 @@ public class Cashier implements Payment {
     private final CustomerFinder finder;
     private final TransactionCreator transactionCreator;
 
+    private final ReservationCreator reservationCreator;
+
     @Autowired
-    public Cashier(Bank bankProxy, CustomerFinder finder, TransactionCreator transactionCreator) {
+    public Cashier(Bank bankProxy, CustomerFinder finder, TransactionCreator transactionCreator, ReservationCreator reservationCreator) {
         this.bankProxy = bankProxy;
         this.finder = finder;
         this.transactionCreator = transactionCreator;
+        this.reservationCreator = reservationCreator;
     }
 
     @Override
@@ -37,5 +43,10 @@ public class Cashier implements Payment {
         }
         String payment = bankProxy.pay(customer, amount).orElseThrow(() -> new PaymentException(customer.getName(), amount));
         return transactionCreator.createTransaction(customer, amount, payment);
+    }
+
+    @Override
+    public Reservation payReservationFromCart(Customer customer, Item item) throws PaymentException {
+        return reservationCreator.createReservation(customer,item);
     }
 }
