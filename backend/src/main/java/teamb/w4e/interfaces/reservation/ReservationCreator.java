3edca87887da4b460af6ef16a6389cd35aecd61f
Orfila Marie <marie.orfila@etu.univ-cotedazur.fr>
2024-02-22 23:51:54 +0100

    fix: remove payment transaction #18

--- backend/src/main/java/teamb/w4e/interfaces/reservation/ReservationCreator.java
+++ backend/src/main/java/teamb/w4e/interfaces/reservation/ReservationCreator.java
@@ -3,9 +3,10 @@ package teamb.w4e.interfaces.reservation;
 import teamb.w4e.entities.Customer;
 import teamb.w4e.entities.Item;
 import teamb.w4e.entities.Reservation;
+import teamb.w4e.entities.Transaction;
 
 public interface ReservationCreator {
-    Reservation createReservation(Customer customer, Item item);
+    Reservation createReservation(Customer customer, Item item, Transaction transaction);
 
 
 }
