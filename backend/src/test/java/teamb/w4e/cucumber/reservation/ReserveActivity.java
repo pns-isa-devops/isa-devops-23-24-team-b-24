76e2eab921f326576778460e5cf5602facc9751e
Arnaud <arnaud.dumanois@etu.univ-cotedazur.fr>
2024-02-23 23:22:02 +0100

    feat: setup RunnerIT for Catalog Test #22

--- backend/src/test/java/teamb/w4e/cucumber/reservation/ReserveActivity.java
+++ backend/src/test/java/teamb/w4e/cucumber/reservation/ReserveActivity.java
@@ -1,14 +1,21 @@
 package teamb.w4e.cucumber.reservation;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import teamb.w4e.entities.Customer;
+import teamb.w4e.exceptions.PaymentException;
 import teamb.w4e.interfaces.Bank;
 import teamb.w4e.repositories.CustomerRepository;
 import teamb.w4e.repositories.TransactionRepository;
 import teamb.w4e.repositories.reservation.ReservationRepository;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @Transactional
 public class ReserveActivity {
@@ -26,9 +33,17 @@ public class ReserveActivity {
     @Autowired
     private ReservationRepository reservationRepository;
 
+    @Before
+    public void settingUpContext() throws PaymentException {
+        customerRepository.deleteAll();
+        transactionRepository.deleteAll();
+        reservationRepository.deleteAll();
+        when(bankMock.pay(any(Customer.class), any(Double.class))).thenReturn(Optional.of("1234567890"));
+    }
+
     @Given("a customer named {string} with the credit card number {string}")
     public void aCustomerNamedWithTheCreditCardNumber(String arg0, String arg1) {
-        // always say tru for the test need to change
+        // always say true for the test need to change
         assertTrue(true);
     }
 }
