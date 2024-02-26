a17ac190530e5ad771f79e809707b1c690ffb13f
ClementREMY2 <clement.remy@etu.univ-cotedazur.fr>
2024-02-26 11:07:06 +0100

    feat: add correct tests to cartHandler with scheduler #8

--- backend/src/test/java/teamb/w4e/components/CartHandlerTest.java
+++ backend/src/test/java/teamb/w4e/components/CartHandlerTest.java
@@ -8,16 +8,18 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 import teamb.w4e.entities.Activity;
 import teamb.w4e.entities.Customer;
+import teamb.w4e.entities.Item;
 import teamb.w4e.exceptions.IdNotFoundException;
 import teamb.w4e.exceptions.NonValidDateForActivity;
 import teamb.w4e.interfaces.*;
 import teamb.w4e.repositories.ActivityCatalogRepository;
 import teamb.w4e.repositories.CustomerRepository;
 
+import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Transactional
@@ -35,7 +37,7 @@ class CartHandlerTest {
     @Autowired
     private Payment payment;
 
-    @MockBean
+    @Autowired
     private Scheduler scheduler;
 
     private Customer customer;
@@ -54,6 +56,11 @@ class CartHandlerTest {
         activity = new Activity("Tennis", "Tennis court", Set.of());
         customerRepository.save(customer);
         activityCatalogRepository.save(activity);
+        Item i = new Item(activity, "07-11 21:30");
+        Set<Item> e = new HashSet<>();
+        e.add(i);
+        customer.getCaddy().setActivities(e);
+
 
     }
 
