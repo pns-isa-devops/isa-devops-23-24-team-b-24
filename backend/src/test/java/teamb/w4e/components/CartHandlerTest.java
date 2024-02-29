a678cb39f614ee5cc966a828fffc3c54844fc91a
Orfila Marie <marie.orfila@etu.univ-cotedazur.fr>
2024-02-29 14:49:51 +0100

    fix: modify import path #8

--- backend/src/test/java/teamb/w4e/components/CartHandlerTest.java
+++ backend/src/test/java/teamb/w4e/components/CartHandlerTest.java
@@ -7,11 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import teamb.w4e.entities.Activity;
 import teamb.w4e.entities.Customer;
-import teamb.w4e.entities.Item;
+import teamb.w4e.entities.cart.Item;
+import teamb.w4e.entities.cart.TimeSlotItem;
+import teamb.w4e.exceptions.CustomerIdNotFoundException;
 import teamb.w4e.exceptions.IdNotFoundException;
 import teamb.w4e.exceptions.NonValidDateForActivity;
 import teamb.w4e.interfaces.*;
-import teamb.w4e.repositories.ActivityCatalogRepository;
+import teamb.w4e.repositories.catalog.ActivityCatalogRepository;
 import teamb.w4e.repositories.CustomerRepository;
 import java.util.HashSet;
 import java.util.Set;
@@ -49,10 +51,10 @@ class CartHandlerTest {
     @BeforeEach
     void setUp() {
         customer = new Customer("Marcus", "1230896983");
-        activity = new Activity("Tennis", "Tennis court", Set.of());
+        activity = new Activity("Tennis", "Tennis court",123, Set.of());
         customerRepository.save(customer);
         activityCatalogRepository.save(activity);
-        Item i = new Item(activity, "07-11 21:30");
+        TimeSlotItem i = new TimeSlotItem(activity, "07-11 21:30");
         Set<Item> e = new HashSet<>();
         e.add(i);
         customer.getCaddy().setActivities(e);
@@ -61,10 +63,10 @@ class CartHandlerTest {
     }
 
     @Test
-    void update() throws NonValidDateForActivity, IdNotFoundException {
-        cartModifier.update(customer.getId(), activity, "07-11 21:30");
-        assertTrue(customerFinder.retrieveCustomer(customer.getId()).getCaddy().getActivities().size() == 1);
-        assertThrows(NonValidDateForActivity.class, () -> cartModifier.update(customer.getId(), activity, "04-03 10:40"));
+    void update() throws NonValidDateForActivity, IdNotFoundException, CustomerIdNotFoundException {
+        cartModifier.timeSlotUpdate(customer.getId(), activity, "07-11 21:30");
+        assertEquals(1, customerFinder.retrieveCustomer(customer.getId()).getCaddy().getActivities().size());
+        assertThrows(NonValidDateForActivity.class, () -> cartModifier.timeSlotUpdate(customer.getId(), activity, "04-03 10:40"));
     }
 
 
