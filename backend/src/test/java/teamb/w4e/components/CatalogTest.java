0be7ed09887cb74bd723aa81852ef06a51375067
Arnaud <arnaud.dumanois@etu.univ-cotedazur.fr>
2024-03-21 19:06:37 +0100

    test: delete operation fonctional #44

--- backend/src/test/java/teamb/w4e/components/CatalogTest.java
+++ backend/src/test/java/teamb/w4e/components/CatalogTest.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 import teamb.w4e.entities.catalog.Activity;
 import teamb.w4e.entities.catalog.Advantage;
 import teamb.w4e.entities.catalog.AdvantageType;
+import teamb.w4e.exceptions.IdNotFoundException;
 import teamb.w4e.interfaces.AdvantageFinder;
 import teamb.w4e.interfaces.AdvantageRegistration;
 import teamb.w4e.interfaces.leisure.ActivityFinder;
@@ -70,6 +71,24 @@ class CatalogTest {
         Activity activity = activityRegistration.register("name", "description", 10.0, Collections.emptySet());
         Optional<Activity> found = activityFinder.findActivityByName(activity.getName());
         assertTrue(found.isPresent());
+    }
+
+    @Test
+    void deleteActivity() throws IdNotFoundException {
+        Activity activity = activityRegistration.register("name", "description", 10.0, Collections.emptySet());
+        assertTrue(activityFinder.findActivityByName(activity.getName()).isPresent());
+        activityRegistration.deleteActivity(activity.getId());
+        Optional<Activity> found = activityFinder.findActivityByName(activity.getName());
+        assertFalse(found.isPresent());
+    }
+
+    @Test
+    void deleteService() throws IdNotFoundException {
+        Activity activity = activityRegistration.register("name", "description", 10.0, Collections.emptySet());
+        assertTrue(activityFinder.findActivityByName(activity.getName()).isPresent());
+        activityRegistration.deleteActivity(activity.getId());
+        Optional<Activity> found = activityFinder.findActivityByName(activity.getName());
+        assertFalse(found.isPresent());
     }
 
 }
